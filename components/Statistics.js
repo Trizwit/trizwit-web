c0cb1b0a50063d63de126d0847229484e7102a0a
aswinraj <aswinrajkailath@gmail.com>
2022-08-27 20:11:28 +0530

    added statistics

---
+++ components/Statistics.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Statistics = () => {
+  return (
+    <div name="about" className="w-full my-32">
+      <div className="max-w-[1240px] mx-auto">
+        <div className="text-center">
+          <h2 className="text-5xl font-bold">
+            Trusted by companies across the world
+          </h2>
+          <p className="text-3xl py-6 text-gray-500">
+            The best in class solutions designed to meet your requirements.
+          </p>
+        </div>
+
+        <div className="grid md:grid-cols-3 gap-1 px-2 text-center">
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">12</p>
+            <p className="text-gray-400 mt-2">Project</p>
+          </div>
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">3</p>
+            <p className="text-gray-400 mt-2">Partner Companies</p>
+          </div>
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">300</p>
+            <p className="text-gray-400 mt-2">Users</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Statistics from "../components/Statistics";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Statistics />
       <Footer />
     </>
   );
