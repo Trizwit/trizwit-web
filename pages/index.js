40d5169e05a46c2fff6129a3186d5301dba5a75e
aswinraj <aswinrajkailath@gmail.com>
2022-08-28 21:49:53 +0530

    added services

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Services from "../components/Services";
 import Statistics from "../components/Statistics";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+      <Services />
       <Statistics />
       <Footer />
     </>
