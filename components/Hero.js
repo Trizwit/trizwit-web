cf1158b2999e2afc9c9c5b521412fb90f3494a9e
aswinraj <aswinrajkailath@gmail.com>
2022-08-29 22:23:15 +0530

    modified icons and content

--- components/Hero.js
+++ components/Hero.js
@@ -1,111 +1,52 @@
 import React from "react";
 import bgImg from "../assets/images/hero_mushroom.png";
 import Image from "next/image";
-const Web3Icon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-      />
-    </svg>
-  );
-};
-const MetaIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-      />
-    </svg>
-  );
-};
-const AiIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-      />
-    </svg>
-  );
-};
-const IotIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 002.25-2.25V6.75a2.25 2.25 0 00-2.25-2.25H6.75A2.25 2.25 0 004.5 6.75v10.5a2.25 2.25 0 002.25 2.25zm.75-12h9v9h-9v-9z"
-      />
-    </svg>
-  );
-};
+import { SiHiveBlockchain } from "react-icons/si";
+import { BiNetworkChart } from "react-icons/bi";
+import { TbAugmentedReality } from "react-icons/tb";
+import { GiBrain } from "react-icons/gi";
+
 const Hero = () => {
   return (
     <div name="home" className="w-full h-screen  flex flex-col justify-between">
       <div className="grid md:grid-cols-2 max-w-[1240px] mx-auto mt-6">
         <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
-          <p className="text-2xl">The WEB3 COMPANY</p>
-          <h1 className="py-3 text-5xl md:text-7xl font-bold">Trizwit</h1>
-          <p className="text-2xl">Have a look at our services.</p>
+          {/* <p className="text-2xl">The WEB3 COMPANY</p> */}
+          <h1 className="py-3 text-5xl text-indigo-400 md:text-6xl font-bold">
+            WEB 3 Development and Consulting
+          </h1>
+          <p className="text-2xl">
+            Blockchain | DeFi | Metaverse | Web3 Gaming | NFTs | DAOs | Tooling
+          </p>
           <button className="py-3 px-6 bg-indigo-600 rounded text-white sm:w-[60%] my-4 ">
-            Services
+            Our Services
           </button>
         </div>
         <div>
           <Image className="w-full" src={bgImg} alt="/" />
         </div>
         <div
-          className="absolute flex flex-col py-8 md:min-w-[760px] bottom-[2%]
+          className="absolute flex flex-col pt-4 pb-6 md:min-w-[760px] bottom-[2%]
             mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-white
             border border-slate-300 rounded-xl text-center shadow-xl"
         >
-          <p className="text-3xl font-semibold text-indigo-600 mb-4">Domains</p>
+          <p className="text-3xl font-semibold text-indigo-600 mb-4">
+            Our Domains
+          </p>
           <div className="flex justify-between flex-wrap px-4">
-            <p className="flex px-4 py-2 text-slate-500">
-              <Web3Icon className="h-6 text-indigo-600" /> Web 3.0
+            <p className="flex px-4 py-2 text-slate-500 text-lg">
+              <SiHiveBlockchain className="mr-2 h-7 w-7 text-indigo-600" /> Web
+              3.0
+            </p>
+            <p className="flex px-4 py-2 text-slate-500 text-lg">
+              <TbAugmentedReality className="mr-2 h-8 w-8 text-indigo-600" />{" "}
+              Metaverse
             </p>
             <p className="flex px-4 py-2 text-slate-500">
-              <MetaIcon className="h-6 text-indigo-600" /> Metaverse
+              <GiBrain className="mr-2 h-8 w-8 text-indigo-600" /> AI
             </p>
             <p className="flex px-4 py-2 text-slate-500">
-              <AiIcon className="h-6 text-indigo-600" /> AI
-            </p>
-            <p className="flex px-4 py-2 text-slate-500">
-              <IotIcon className="h-6 text-indigo-600" /> IoT
+              <BiNetworkChart className="mr-2 h-8 w-8 text-indigo-600" /> IoT
             </p>
           </div>
         </div>
