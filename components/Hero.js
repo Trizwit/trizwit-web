0d23baebb920d83be007133dd787b575321aad36
aswinraj <aswinrajkailath@gmail.com>
2022-08-30 21:38:37 +0530

    modified hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,10 @@
 import React from "react";
-import bgImg from "../assets/images/hero_mushroom.png";
+import bgImg from "../assets/images/hero_bg.png";
 import Image from "next/image";
 import { SiHiveBlockchain } from "react-icons/si";
 import { BiNetworkChart } from "react-icons/bi";
 import { TbAugmentedReality } from "react-icons/tb";
-import { GiBrain } from "react-icons/gi";
+import { MdApps } from "react-icons/md";
 
 const Hero = () => {
   return (
@@ -12,13 +12,13 @@ const Hero = () => {
       <div className="grid md:grid-cols-2 max-w-[1240px] mx-auto mt-6">
         <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
           {/* <p className="text-2xl">The WEB3 COMPANY</p> */}
-          <h1 className="py-3 text-5xl text-indigo-400 md:text-6xl font-bold">
-            WEB 3 Development and Consulting
+          <h1 className="py-3 text-5xl  md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500  to-purple-600">
+            WEB 3.0 Development and Consulting
           </h1>
-          <p className="text-2xl">
+          <p className="text-2xl text-slate-800">
             Blockchain | DeFi | Metaverse | Web3 Gaming | NFTs | DAOs | Tooling
           </p>
-          <button className="py-3 px-6 bg-indigo-600 rounded text-white sm:w-[60%] my-4 ">
+          <button className="py-3 px-6 bg-indigo-600 rounded text-2xl text-white  my-4 ">
             Our Services
           </button>
         </div>
@@ -35,17 +35,17 @@ const Hero = () => {
           </p>
           <div className="flex justify-between flex-wrap px-4">
             <p className="flex px-4 py-2 text-slate-500 text-lg">
-              <SiHiveBlockchain className="mr-2 h-7 w-7 text-indigo-600" /> Web
-              3.0
+              <SiHiveBlockchain className="mr-2 h-7 w-7 text-indigo-600" />{" "}
+              Blockchain
             </p>
             <p className="flex px-4 py-2 text-slate-500 text-lg">
               <TbAugmentedReality className="mr-2 h-8 w-8 text-indigo-600" />{" "}
               Metaverse
             </p>
-            <p className="flex px-4 py-2 text-slate-500">
-              <GiBrain className="mr-2 h-8 w-8 text-indigo-600" /> AI
+            <p className="flex px-4 py-2 text-slate-500 text-lg">
+              <MdApps className="mr-2 h-8 w-8 text-indigo-600" /> Web & Native
             </p>
-            <p className="flex px-4 py-2 text-slate-500">
+            <p className="flex px-4 py-2 text-slate-500 text-lg">
               <BiNetworkChart className="mr-2 h-8 w-8 text-indigo-600" /> IoT
             </p>
           </div>
