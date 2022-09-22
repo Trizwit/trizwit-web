69344433f3c28aff1c98cdcea60451767ebcf1b9
aswinraj <aswinrajkailath@gmail.com>
2022-09-22 23:13:29 +0530

    added Contact form

---
+++ components/Contact.js
@@ -0,0 +1,19 @@
+import { Widget } from "@typeform/embed-react";
+
+const Contact = () => {
+  return (
+    <div className="mx-auto max-w-[1200px] py-4 my-6 items-center">
+      <div className="">
+        <h1 className="text-5xl font-bold text-center pb-8 text-indigo-500">
+          {"Contact Us "}
+        </h1>
+        <Widget
+          id="Vl5flN4C"
+          className="mx-auto max-w-[1000px] h-[500px] px-2"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
@@ -13,6 +14,7 @@ export default function Home() {
       <Hero />
       <Services />
       <Statistics />
+      <Contact />
       <Footer />
     </>
   );
