587295586e78ee2c05c3842fe84803c24d909295
Athul <athulakumar10@gmail.com>
2023-06-22 14:11:31 +0530

    products in main nav added

--- components/Footer.js
+++ components/Footer.js
@@ -4,25 +4,25 @@ import { FaLinkedinIn, FaGithub, FaTwitter, FaMedium } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-full mt-24 bg-slate-900 text-gray-300 py-y px-2">
+    <div className="w-full mt-24 bg-gray-100 drop-shadow-lg text-gray-300 py-y px-2">
       
 
       <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500">
       <div className="flex items-center">
-      <img src="trizlogo.png" alt="Logo" className="h-8 w-8 md:h-12 md:w-12 ml-2 md:ml-4" />
+      <img src="trizwitlogo.png" alt="Logo" className="h-8 w-8 md:h-12 md:w-12 ml-2 md:ml-4" />
         <p className="ml-2 md:ml-4 py-4">2022 Trizwit Labs Pvt Ltd. All rights reserved</p>
         </div>
         <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
-          <a href="https://www.linkedin.com/company/trizwit/"  >
+          <a href="https://www.linkedin.com/company/trizwit/" className="hover:text-[#0277B5]" >
             <FaLinkedinIn />
           </a>
-          <a href="https://twitter.com/trizwit?s=20&t=P2u10VMBS-FuDIQClE-m3Q"  >
+          <a href="https://twitter.com/trizwit?s=20&t=P2u10VMBS-FuDIQClE-m3Q" className="hover:text-[#1B9DF0]"  >
             <FaTwitter />
           </a>
-          <a href="https://github.com/Trizwit"  >
+          <a href="https://github.com/Trizwit" className="hover:text-black" >
             <FaGithub />
           </a>
-          <a href="https://medium.com/@trizwit"  >
+          <a href="https://medium.com/@trizwit" className="hover:text-black" >
             <FaMedium />
           </a>
         </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,14 @@ import { useRouter } from "next/router";
 import { FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 
+
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
   const [nav, setNav] = useState(false);
   const router = useRouter();
   const handleClick = () => setNav(!nav);
@@ -15,47 +22,77 @@ const Navbar = () => {
       <div className="flex justify-between items-center">
         <div className="flex items-center">
           <div className="flex items-center">
-        <img src="trizlogo.png" alt="Logo" className="h-12 w-12 md:h-16 md:w-16 ml-2 md:ml-4" />
+        <img src="trizwitlogo.png" alt="Logo" className="h-12 w-12 md:h-16 md:w-16 ml-2 md:ml-4" />
         <h2 className="text-3xl font-bold ml-0  px-4 py-4 hover:text-indigo-500 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600">
           Trizwit.
         </h2>
         </div>
-          <ul className="hidden md:flex items-center pl-6">
-            <li className="text-black px-4 font-semibold">
+          
+        </div>
+        <div className="hidden md:flex  md:mr-4 lg:mr-24">
+        
+          <ul className="hidden md:flex items-center ">
+            <li className="text-black hover:text-blue-500 px-4 font-semibold">
               <Link href="/">Home</Link>
             </li>
-            <li className="text-black px-4 font-semibold">
+            <li className="text-black hover:text-blue-500 px-4 font-semibold">
               <Link href="#services">Services</Link>
             </li>
-            <li className="text-black px-4 font-semibold">
+
+            <button
+  className="px-4 relative"
+  onMouseEnter={handleToggle}
+  onMouseLeave={handleToggle}
+>
+  <li className="text-black hover:text-blue-500 px-4 font-semibold">
+    <Link href="about">Products</Link>
+  </li>
+  {isOpen && (
+    <div className="absolute justify-items-center left-1/2 transform -translate-x-1/2  bg-white px-2 py-0 rounded-md shadow-lg">
+      <a
+        href="https://pindown.trizwit.com/"
+        className="flex w-64 px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-blue-500"
+      >
+        {/* <img src="trizwitlogo.png" alt="Logo" className="block h-4 w-4 md:h-8 md:w-8 mr-2" /> */}
+        <div className="justify-items-center">
+          <span className="block">Pindown</span>
+          <span className="block text-sm text-gray-500">
+            Tokenise and verify certificates
+          </span>
+        </div>
+      </a>
+      <a
+        href="https://blockiot.trizwit.com/"
+        className="flex w-64 px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-blue-500"
+      >
+        {/* <img src="trizwitlogo.png" alt="Logo" className="block h-4 w-4 md:h-8 md:w-8 mr-2" /> */}
+        <div className="text-center" >
+          <span className="block">BlockIoT</span>
+          <span className="block text-sm text-gray-500">
+            Blockchain-IoT integration simplified
+          </span>
+        </div>
+      </a>
+      <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+        Item 3
+      </a>
+    </div>
+  )}
+</button>
+
+
+            <li className="text-black hover:text-blue-500 px-4 font-semibold">
               <Link href="about">About</Link>
             </li>
-            {/* <li className="text-black px-4 font-semibold">
-            <Link href='team' >Team</Link>
-            </li> */}
-            <li className="text-black px-4 font-semibold">
+
+            
+            
+
+            <li className="text-black hover:text-blue-500 px-4 font-semibold">
               <Link href="#contact">Contact</Link>
             </li>
           </ul>
-        </div>
-        <div className="hidden md:flex">
-          {/* <button className="mx-2 border-2 rounded-full border-indigo-500 text-indigo-500 px-4 py-2 hover:shadow-2xl">
-            {"Sign In"}
-          </button> */}
-          {/* <a href="#contact">
-            <button className="mx-2 border-2 rounded-lg  text-white bg-indigo-500 px-4 py-2 hover:shadow-2xl">
-              {"Get in Touch"}
-            </button>
-          </a> */}
-          <a href="https://www.linkedin.com/company/trizwit/"  >
-            <FaLinkedinIn className="h-8 w-8 mx-4 text-blue-700" />
-          </a>
-          <a
-            href="https://twitter.com/trizwit?s=20&t=P2u10VMBS-FuDIQClE-m3Q"
-             
-          >
-            <FaTwitter className="h-8 w-8 ml-4 mr-6 text-blue-500" />
-          </a>
+         
         </div>
         <div className="md:hidden" onClick={handleClick}>
           {!nav ? (
@@ -75,9 +112,9 @@ const Navbar = () => {
         <li className="border-b border-zinc-400 py-2 mb-2 hover:text-indigo-500">
          <Link href="about">About</Link>
         </li>
-        {/* <li className="border-b border-zinc-400 py-2 mb-2 hover:text-indigo-500">
-          Team
-        </li> */}
+        <li className="border-b border-zinc-400 py-2 mb-2 hover:text-indigo-500">
+        <Link href="/">Products</Link>
+        </li>
         <li className="border-b border-zinc-400 py-2 mb-2 hover:text-indigo-500">
          <Link href="#contact">Contact</Link>
         </li>
@@ -87,7 +124,7 @@ const Navbar = () => {
               {"Twitter "}
             </button>
           </a> */}
-          <a
+          {/* <a
             href="https://www.linkedin.com/company/trizwit/"
              
             className="mx-4 h-6 w-6 text-blue-700"
@@ -100,7 +137,7 @@ const Navbar = () => {
             className="mx-4 h-6 w-6 text-blue-500"
           >
             <FaTwitter className="h-6 w-6" />
-          </a>
+          </a> */}
         </div>
       </ul>
     </div>
