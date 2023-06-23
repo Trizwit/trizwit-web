9c75db5e69d18c21386d234d73eeda768b1ba92b
Athul <athulakumar10@gmail.com>
2023-06-24 00:01:59 +0530

    code updated

--- components/Contact.js
+++ components/Contact.js
@@ -1,19 +1,212 @@
-import { Widget } from "@typeform/embed-react";
+// import { Widget } from "@typeform/embed-react";
 
-const Contact = () => {
+// const Contact = () => {
+//   return (
+//     <div id="contact" className="mx-auto max-w-[1200px] py-4 my-6 items-center">
+//       <div className="">
+//         <h1 className="text-5xl font-bold text-center pb-8 text-indigo-500">
+//           {"Contact Us "}
+//         </h1>
+//         <Widget
+//           id="Vl5flN4C"
+//           className="mx-auto max-w-[1000px] h-[500px] px-2"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Contact;
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import useWeb3Forms from "@web3forms/react";
+{/*import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Switch } from '@headlessui/react'*/}
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Contact() {
+  const [agreed, setAgreed] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    control,
+    setValue,
+    formState: { errors, isSubmitSuccessful, isSubmitting },
+  } = useForm({
+    mode: "onTouched",
+  });
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [message, setMessage] = useState(false);
+  const apiKey = process.env.PUBLIC_ACCESS_KEY || "194709fc-73f7-45d3-bca6-2b06398661a5";
+
+  const { submit: onSubmit } = useWeb3Forms({
+    access_key: apiKey,
+    settings: {
+      from_name: "Enquiry",
+      subject: "New Contact Message from your Trizwit.com",
+    },
+    onSuccess: (msg, data) => {
+      setIsSuccess(true);
+      setMessage(msg);
+      reset();
+    },
+    onError: (msg, data) => {
+      setIsSuccess(false);
+      setMessage(msg);
+    },
+  });
+  
+
   return (
-    <div id="contact" className="mx-auto max-w-[1200px] py-4 my-6 items-center">
-      <div className="">
-        <h1 className="text-5xl font-bold text-center pb-8 text-indigo-500">
-          {"Contact Us "}
-        </h1>
-        <Widget
-          id="Vl5flN4C"
-          className="mx-auto max-w-[1000px] h-[500px] px-2"
+    <div id="contact" className="isolate bg-gray-100 px-6 py-24 sm:py-32 lg:px-8">
+      <div
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
         />
       </div>
-    </div>
-  );
-};
+      <div className="mx-auto max-w-2xl text-center">
+      <h1 className="py-3 text-5xl  md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500  to-purple-600">Contact Us</h1>
+        {/* <p className="mt-2 text-lg leading-8 text-black-600 font-bold">
+          Get In Touch With Us
+        </p> */}
+      </div>
+      
+      <form onSubmit={handleSubmit(onSubmit)} className="mx-auto mt-16 max-w-xl sm:mt-20">
+        <input
+          type="checkbox"
+          id=""
+          className="hidden"
+          style={{ display: "none" }}
+          {...register("botcheck")}></input>
 
-export default Contact;
+        <div className="mb-5">
+          <input
+            type="text"
+            placeholder="Full Name"
+            autoComplete="false"
+            className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6  ${
+              errors.name
+                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+            }`}
+            {...register("name", {
+              required: "Full name is required",
+              maxLength: 80,
+            })}
+          />
+          {errors.name && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.name.message}</small>
+            </div>
+          )}
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="email_address" className="sr-only">
+            Email Address
+          </label>
+          <input
+            id="email_address"
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            autoComplete="false"
+            className={`block w-full rounded-md border-1 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6  ${
+              errors.email
+                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+            }`}
+            {...register("email", {
+              required: "Enter your email",
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: "Please enter a valid email",
+              },
+            })}
+          />
+          {errors.email && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.email.message}</small>
+            </div>
+          )}
+        </div>
+
+        <div className="mt-2.5">
+        {/*<label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+              Message
+          </label>*/}
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6  ${
+              errors.message
+                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+            }`}
+            {...register("message", {
+              required: "Enter your Message",
+            })}
+          />
+          {errors.message && (
+            <div className="mt-1 text-red-600">
+              {" "}
+              <small>{errors.message.message}</small>
+            </div>
+          )}
+        </div>
+
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-gradient-to-r from-blue-500  to-purple-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            {isSubmitting ? (
+            <svg
+              className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24">
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+          ) : (
+            "Let's talk"
+          )}
+            
+          </button>
+        </div>
+      </form>
+      {isSubmitSuccessful && isSuccess && (
+        <div className="mt-3 text-sm text-center text-green-500">
+          {message || "Success. Message sent successfully"}
+        </div>
+      )}
+      {isSubmitSuccessful && !isSuccess && (
+        <div className="mt-3 text-sm text-center text-red-500">
+          {message || "Something went wrong. Please try later."}
+        </div>
+      )}
+    </div>
+  )
+}
